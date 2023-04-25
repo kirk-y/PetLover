30942109c2fa56978b2c691fc46a8d86ece9a6a2
kirk-y <2087629236@qq.com>
2023-04-25 21:07:34 +0800

    Map in Adoption have done, but there is is a problem that backing from map will kill the Adoption Activity which meas we can't back in formal way.

---
+++ app/src/main/java/com/example/petlover/Functions/Adopte/PetMap.java
@@ -0,0 +1,271 @@
+package com.example.petlover.Functions.Adopte;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.MapsInitializer;
+import com.amap.api.services.core.ServiceSettings;
+import com.example.petlover.R;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class PetMap extends AppCompatActivity  implements AMapLocationListener, LocationSource{
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        setContentView(R.layout.activity_pet_map);
+
+        location_content = findViewById(R.id.location_content);
+        mapView = findViewById(R.id.map_view);
+        System.out.println("------------------------- " + mapView);
+        if (mapView == null) {
+            showMsg("mapView is null");
+        }
+        mapView.onCreate(savedInstanceState);
+
+        Context context = this;
+        //定位隐私政策同意
+        AMapLocationClient.updatePrivacyShow(context,true,true);
+        AMapLocationClient.updatePrivacyAgree(context,true);
+        //地图隐私政策同意
+        MapsInitializer.updatePrivacyShow(context,true,true);
+        MapsInitializer.updatePrivacyAgree(context,true);
+        //搜索隐私政策同意
+        ServiceSettings.updatePrivacyShow(context,true,true);
+        ServiceSettings.updatePrivacyAgree(context,true);
+
+        initLocation();
+        initMap(savedInstanceState);
+        checkingAndroidVersion();
+
+        ImageButton bnt_back = findViewById(R.id.bnt_map_back);
+        bnt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private TextView location_content;
+    //地图控制器
+    private AMap aMap = null;
+    //位置更改监听
+    private LocationSource.OnLocationChangedListener mListener;
+
+    /**
+     * 初始化地图
+     * @param savedInstanceState
+     */
+    private void initMap(Bundle savedInstanceState) {
+        mapView = findViewById(R.id.map_view);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(savedInstanceState);
+        //初始化地图控制器对象
+        aMap = mapView.getMap();
+
+        // 设置定位监听
+        aMap.setLocationSource(this);
+        // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        aMap.setMyLocationEnabled(true);
+    }
+
+
+    /**
+     * 检查Android版本
+     */
+    private void checkingAndroidVersion() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            //Android6.0及以上先获取权限再定位
+            requestPermission();
+        }else {
+            //Android6.0以下直接定位
+            mLocationClient.startLocation();
+        }
+    }
+
+    //请求权限码
+    private static final int REQUEST_PERMISSIONS = 9527;
+    /**
+     * 动态请求权限
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSIONS)
+    private void requestPermission() {
+        String[] permissions = {
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        if (EasyPermissions.hasPermissions(this, permissions)) {
+            //true 有权限 开始定位
+            System.out.println("已获得权限，可以定位啦！");
+            // 启动定位
+            mLocationClient.startLocation();
+        } else {
+            //false 无权限
+            EasyPermissions.requestPermissions(this, "需要权限", REQUEST_PERMISSIONS, permissions);
+        }
+    }
+    /**
+     * 请求权限结果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //设置权限请求结果
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    /**
+     * Toast提示
+     * @param msg 提示内容
+     */
+    private void showMsg(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+    /**
+     * 初始化定位
+     */
+    private void initLocation() {
+        //初始化定位
+        try {
+            mLocationClient = new AMapLocationClient(getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (mLocationClient != null) {
+            //设置定位回调监听
+            mLocationClient.setLocationListener((AMapLocationListener) this);
+            //初始化AMapLocationClientOption对象
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //获取最近3s内精度最高的一次定位结果：
+            //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+            mLocationOption.setOnceLocationLatest(true);
+            //设置是否返回地址信息（默认返回地址信息）
+            mLocationOption.setNeedAddress(true);
+            //设置定位请求超时时间，单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+            mLocationOption.setHttpTimeOut(20000);
+            //关闭缓存机制，高精度定位会产生缓存。
+            mLocationOption.setLocationCacheEnable(false);
+            //给定位客户端对象设置定位参数
+            mLocationClient.setLocationOption(mLocationOption);
+        }
+    }
+    /**
+     * 接收异步返回的定位结果
+     *
+     * @param aMapLocation
+     */
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //地址
+                String address = aMapLocation.getAddress();
+                String latitude = String.valueOf(aMapLocation.getLatitude());
+                String longitude = String.valueOf(aMapLocation.getLongitude());
+                StringBuilder stringBuffer = new StringBuilder();
+                stringBuffer.append("纬度：").append(latitude).append("\n");
+                stringBuffer.append("经度：").append(longitude).append("\n");
+                stringBuffer.append("地址：").append(address).append("\n");
+                location_content.setText(stringBuffer.toString());
+                Log.d("Location", stringBuffer.toString());
+
+                if (mListener != null){
+                    mListener.onLocationChanged(aMapLocation);
+                }
+            } else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //停止定位后，本地定位服务并不会被销毁
+        mLocationClient.stopLocation();
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState);
+    }
+    /**
+     * 激活定位
+     */
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mLocationClient == null) {
+            mLocationClient.startLocation();//启动定位
+        }
+    }
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
+    }
+
+}
--- app/src/main/java/com/example/petlover/Functions/Adoption.java
+++ app/src/main/java/com/example/petlover/Functions/Adoption.java
@@ -2,8 +2,13 @@ package com.example.petlover.Functions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.example.petlover.Functions.Adopte.PetMap;
 import com.example.petlover.R;
 
 public class Adoption extends AppCompatActivity {
@@ -12,5 +17,16 @@ public class Adoption extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adoption);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        ImageButton bnt_adoption_pet1 = findViewById(R.id.bnt_adoption_pet1);
+        bnt_adoption_pet1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Adoption.this, PetMap.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
